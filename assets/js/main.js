5b0a70f9b1a34a5cbfd5974b44ea6d211a02901a
Josh <heyjoshkruse@gmail.com>
2019-10-25 12:09:12 -0700

    updated documents page with tech feas.

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,7 +73,7 @@
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
-
+/**/
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
